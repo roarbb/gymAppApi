065599e2d6d73165b8b5517f83e0b8b2f1dd5b0d
Matej Sajgal <matej@sajgal.com>
2016-09-08 22:37:20 +1000

    + creates new user if not in database

--- src/models/user.js
+++ src/models/user.js
@@ -1,4 +1,4 @@
-import {decrypt} from '../lib/util'
+import {decrypt, generateRandomHash} from '../lib/util'
 
 const User = {
 
@@ -15,13 +15,37 @@ const User = {
 
   getByAesHash(db, id) {
     return new Promise((resolve, reject) => {
-      const sql = `SELECT * FROM user WHERE email = '${decrypt(id)}'`
+      const email = decrypt(id);
+      const sql = `SELECT * FROM user WHERE email = '${email}' LIMIT 1`
        db.query(sql, (err, rows) => {
         if (err) reject(err)
+
+        if (rows.length === 0) {
+          resolve(this.createNewUser(db, email))
+          return true
+        }
 
         resolve(rows[0])
       });
     });
+  },
+
+  createNewUser(db, email) {
+    return new Promise((resolve, reject) => {
+      const hash = generateRandomHash()
+      const data = {hash, email}
+
+      db.query('INSERT INTO user SET ?', data, (err, result) => {
+        if (err) reject(err)
+
+        resolve({
+          id: result.insertId,
+          email,
+          hash
+        })
+      })
+
+    })
   }
 
 }
